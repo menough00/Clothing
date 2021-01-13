9d2f3ee4d9e963e12fbf795c3c52a8919a5264c8
menough00 <nickmenough@gmail.com>
2021-01-13 12:37:13 -0600

    Add sub class Jeans

---
+++ src/Jeans.java
@@ -0,0 +1,16 @@
+
+public class Jeans extends Pants{
+
+	public Jeans(String size, String color) {
+		super(size, color);
+		
+	}
+
+	@Override
+	public String pack() {
+		
+		return "Fold";
+	}
+
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Clothing calvin=new Clothing("S","blue");
 		System.out.println(calvin.wash());
-		Clothing guess=new Pants("32c32","Stone Washed");
+		Clothing guess=new Jeans("32c32","Stone Washed");
 		System.out.println(guess.wash());
 		Clothing boss=new Shirt("XL","Black"); 
 		System.out.println(boss.wash());
